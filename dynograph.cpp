2665bdf0c59cb6f5829a85a3c847de115913cc38
Eric Hein <ehein6@gatech.edu>
2017-09-29 17:24:29 -0400

    Created dynograph exe.

---
+++ dynograph.cpp
@@ -0,0 +1,12 @@
+#include <utils.hpp>
+#include <update.hpp>
+#include <memoryManager.hpp>
+#include <cuStingerDefs.hpp>
+#include <cuStinger.hpp>
+
+#include <dynograph_util/benchmark.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
